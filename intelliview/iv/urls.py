b1881dd1e4b76024f7a7fa475fbc3eb1e2e4a9f9
Kaijun6555 <tankai1719@gmail.com>
2024-03-17 22:02:34 +0800

    Update on previous commit
    small changes to urls & Views

--- intelliview/iv/urls.py
+++ intelliview/iv/urls.py
@@ -2,5 +2,7 @@ from django.urls  import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.index)
+    path('', views.index),
+    path('home/', views.home),
+    path('login/', views.index),
 ]
